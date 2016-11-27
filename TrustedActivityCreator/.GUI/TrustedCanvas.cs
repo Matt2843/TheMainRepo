74603612666f3c7f9b18d24542a79c7941613eb0
Matt <Matt@DESKTOP-0UPRFN0>
2016-11-27 23:48:05 +0100

    Stuff

--- TrustedActivityCreator/.GUI/TrustedCanvas.cs
+++ TrustedActivityCreator/.GUI/TrustedCanvas.cs
@@ -5,19 +5,19 @@ using System.Windows.Media;
 namespace TrustedActivityCreator.GUI {
     class TrustedCanvas : Canvas {
 
-        public static readonly DependencyProperty NumOfVerticalLines = DependencyProperty.Register("NumOfVerticalLines", typeof(int), typeof(TrustedCanvas), new PropertyMetadata(false));
-        public static readonly DependencyProperty NumOfHorizontalLines = DependencyProperty.Register("NumOfHorizontalLines", typeof(int), typeof(TrustedCanvas), new PropertyMetadata(false));
-        public static readonly DependencyProperty LinesThickness = DependencyProperty.Register("LineThickness", typeof(int), typeof(TrustedCanvas), new PropertyMetadata(false));
+        public static readonly DependencyProperty NumOfVerticalLines = DependencyProperty.Register("NumOfVerticalLines", typeof(int), typeof(TrustedCanvas), new UIPropertyMetadata(24));
+        public static readonly DependencyProperty NumOfHorizontalLines = DependencyProperty.Register("NumOfHorizontalLines", typeof(int), typeof(TrustedCanvas), new UIPropertyMetadata(24));
+        public static readonly DependencyProperty LinesThickness = DependencyProperty.Register("LineThickness", typeof(int), typeof(TrustedCanvas), new UIPropertyMetadata(2));
         
-        public int LTVAL {
+        public int LINETHICKNESS {
             get { return (int)GetValue(LinesThickness); }
         }
 
-        public int NOVLVAL {
+        public int NUMBEROFVERTICALLINES {
             get { return (int)GetValue(NumOfVerticalLines); }
         }
 
-        public int NOHLVAL {
+        public int NUMBEROFHORIZONTALLINES {
             get { return (int)GetValue(NumOfHorizontalLines); }
         }
 
@@ -26,14 +26,36 @@ namespace TrustedActivityCreator.GUI {
 
         protected override void OnRender(DrawingContext dc) {
             base.OnRender(dc);
-            CellHeight = (int)ActualHeight / NOVLVAL;
-            CellWidth = (int)ActualWidth / NOHLVAL;
+            CellHeight = (int)ActualHeight / NUMBEROFVERTICALLINES;
+            CellWidth = (int)ActualWidth / NUMBEROFHORIZONTALLINES;
 
             double vOffset = 0, hOffset = 0;
+            float[] dashValues = { 5, 2, 15, 4 };
 
-            Pen pen = new Pen();
-            pen.Thickness = LTVAL;
+            Brush brush = new SolidColorBrush(Colors.Black);
+
+            Pen pen = new Pen(brush, LINETHICKNESS);
+            pen.DashStyle = DashStyles.Dash;
             
+            for(int i = 0; i < NUMBEROFHORIZONTALLINES; i++) {
+                for(int j = 0; j < NUMBEROFVERTICALLINES; j++) {
+                    dc.DrawLine(pen, new Point(hOffset, vOffset), new Point(hOffset, CellHeight + vOffset));
+                    vOffset += CellHeight;
+                }
+                hOffset += CellWidth;
+                vOffset = 0;
+            }
+
+            hOffset = 0;
+
+            for(int i = 0; i < NUMBEROFVERTICALLINES; i++) {
+                for(int j = 0; j < NUMBEROFHORIZONTALLINES; i++) {
+                    dc.DrawLine(pen, new Point(hOffset, vOffset), new Point(CellWidth + hOffset, vOffset));
+                    hOffset += CellWidth;
+                }
+                vOffset += CellHeight;
+                hOffset = 0;
+            }
 
         }
     }
