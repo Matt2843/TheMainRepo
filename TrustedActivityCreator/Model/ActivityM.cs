b63534056e9adc17132ec07aaac478a819b2b57b
Matt <Matt@DESKTOP-0UPRFN0>
2016-11-28 11:56:17 +0100

    Super class added for all shapes.

--- TrustedActivityCreator/Model/ActivityM.cs
+++ TrustedActivityCreator/Model/ActivityM.cs
@@ -1,15 +1,10 @@
 ﻿using GalaSoft.MvvmLight;
-using System;
+
 namespace TrustedActivityCreator.Model {
-    class ActivityM : ObservableObject {
+    class ActivityM : ShapeM {
 
         private int activityId;
         private string activityDescription = "Activity";
-
-        private int width = 100;
-        private int height = 50;
-        private int x;
-        private int y;
 
         public int ActivityId {
             get { return activityId; }
@@ -31,44 +26,6 @@ namespace TrustedActivityCreator.Model {
             }
         }
 
-        public int Width {
-            get { return width; }
-            set {
-                if (value != width) {
-                    width = value;
-                    RaisePropertyChanged();
-                }
-            }
-        }
-
-        public int Height {
-            get { return height; }
-            set {
-                if (value != height) {
-                    height = value;
-                    RaisePropertyChanged();
-                }
-            }
-        }
-
-        public int X {
-            get { return x; }
-            set {
-                if (value != x) {
-                    x = value;
-                    RaisePropertyChanged();
-                }
-            }
-        }
-
-        public int Y {
-            get { return y; }
-            set {
-                if (value != y) {
-                    y = value;
-                    RaisePropertyChanged();
-                }
-            }
-        }
+        
     }
 }
--- TrustedActivityCreator/Model/ShapeM.cs
+++ TrustedActivityCreator/Model/ShapeM.cs
@@ -1,7 +1,52 @@
 ﻿using GalaSoft.MvvmLight;
 
 namespace TrustedActivityCreator.Model {
-	class ShapeM : ObservableObject {
+	public abstract class ShapeM : ObservableObject {
+
+		private int width = 75;
+		private int height = 75;
+		private int x;
+		private int y;
+
+		public int Width {
+			get { return width; }
+			set {
+				if (value != width) {
+					width = value;
+					RaisePropertyChanged();
+				}
+			}
+		}
+
+		public int Height {
+			get { return height; }
+			set {
+				if (value != height) {
+					height = value;
+					RaisePropertyChanged();
+				}
+			}
+		}
+
+		public int X {
+			get { return x; }
+			set {
+				if (value != x) {
+					x = value;
+					RaisePropertyChanged();
+				}
+			}
+		}
+
+		public int Y {
+			get { return y; }
+			set {
+				if (value != y) {
+					y = value;
+					RaisePropertyChanged();
+				}
+			}
+		}
 
 	}
 }
