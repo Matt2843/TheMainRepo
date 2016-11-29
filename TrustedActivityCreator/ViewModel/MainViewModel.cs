e70f6d73d18b0c6e81e7dd9c1eff9fd270bc27be
Cap'n Odin <Cap'n Odin@DESKTOP-7R9F3C2>
2016-11-29 22:57:40 +0100

    uNdOrEdO

--- TrustedActivityCreator/ViewModel/MainViewModel.cs
+++ TrustedActivityCreator/ViewModel/MainViewModel.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using TrustedActivityCreator.Command;
 
 namespace TrustedActivityCreator.ViewModel {
     class MainViewModel {
 
+		private UndoRedoController undoRedoController = UndoRedoController.Instance;
 
-    }
+		public ICommand UndoCommand { get; }
+		public ICommand RedoCommand { get; }
+
+		public MainViewModel() {
+			UndoCommand = new RelayCommand(undoRedoController.Undo, undoRedoController.CanUndo);
+			RedoCommand = new RelayCommand(undoRedoController.Redo, undoRedoController.CanRedo);
+		}
+	}
 }
