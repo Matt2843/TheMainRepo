4e2bb247de19a923fcc0a16ea89cd4aec5b33ff9
Cap'n Odin <Cap'n Odin@DESKTOP-7R9F3C2>
2016-12-01 00:03:02 +0100

    Saved

--- TrustedActivityCreator/View/TrustedCondition.xaml.cs
+++ TrustedActivityCreator/View/TrustedCondition.xaml.cs
@@ -21,5 +21,35 @@ namespace TrustedActivityCreator.View {
 		public TrustedCondition() {
 			InitializeComponent();
 		}
+		private void ActivityDescription_OnKeyDown(object sender, KeyEventArgs e) {
+			if(e.Key == Key.Return) {
+				ActivityDescription_LostFocus(sender, e);
+			}
+		}
+
+		private void ActivityDescription_FocusableChanged(object sender, DependencyPropertyChangedEventArgs e) {
+			ActivityDescription.Cursor = ActivityDescription.Focusable ? Cursors.IBeam : Cursors.Arrow;
+		}
+
+		private void ActivityDescription_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
+			ActivityDescription.BorderThickness = new Thickness(1, 1, 1, 1);
+			ActivityDescription.IsReadOnly = false;
+			ActivityDescription.Focusable = true;
+			ActivityDescription.Focus();
+			ActivityDescription.CaretIndex = ActivityDescription.Text.Length;
+		}
+
+		private void ActivityDescription_LostFocus(object sender, RoutedEventArgs e) {
+			Console.WriteLine("Focus Lost");
+			Condition.Fill = Brushes.White;
+			ActivityDescription.BorderThickness = new Thickness(0, 0, 0, 0);
+			ActivityDescription.Focusable = false;
+			ActivityDescription.IsReadOnly = true;
+		}
+
+		private void Activity_FocusGained(object sender, RoutedEventArgs e) {
+			Condition.Fill = Brushes.BlueViolet;
+
+		}
 	}
 }
